a1899fb9a327c5d1ade6aad5cdb3dc849370cf16
ketan0619 <147227567+ketan0619@users.noreply.github.com>
2026-03-24 21:13:12 +0530

    Added User and Progress models with SQLAlchemy

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+class Progress(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    bmi = db.Column(db.Float)
+    weight = db.Column(db.Float)
