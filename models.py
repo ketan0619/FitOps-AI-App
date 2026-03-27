951d512cb2b78210b6fab08adab375e831bd4c87
ketan0619 <147227567+ketan0619@users.noreply.github.com>
2026-03-27 20:28:54 +0530

    updated the models.py

--- models.py
+++ models.py
@@ -2,10 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100))
     password = db.Column(db.String(100))
+
 
 class Progress(db.Model):
     id = db.Column(db.Integer, primary_key=True)
