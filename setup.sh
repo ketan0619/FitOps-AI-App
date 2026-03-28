262e03ed0639333643033e585a05b988b3797d2d
ketan0619 <147227567+ketan0619@users.noreply.github.com>
2026-03-28 23:07:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -43,4 +43,4 @@ else
 fi
 
 # 3. ALWAYS OUTPUT JOIN COMMAND (Safe to run anytime)
-sudo kubeadm token create --print-join-command | tail -n 1
+sudo kubeadm token create --print-join-command | xargs
