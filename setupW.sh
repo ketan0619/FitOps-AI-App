5ece24e1126b7e941f9fb53987b2b5ca6aeedc02
ketan0619 <147227567+ketan0619@users.noreply.github.com>
2026-04-08 12:41:21 +0530

    Update setupW.sh

--- setupW.sh
+++ setupW.sh
@@ -3,7 +3,6 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
-# 1. CHECK IF ALREADY INSTALLED
 if command -v kubeadm &> /dev/null; then
     echo "Kubernetes is already installed on Worker. Skipping package installation..."
 else
@@ -12,14 +11,11 @@ else
     sudo apt install -y docker.io apt-transport-https ca-certificates curl gpg
     sudo systemctl enable --now docker
 
-    # Disable Swap (Mandatory for Kubernetes)
     sudo swapoff -a
     sudo sed -i '/swap/s/^/#/' /etc/fstab
 
-    # Repository setup - FIXED URL to v1.31
     sudo mkdir -p -m 755 /etc/apt/keyrings
     
-    # Use the full versioned path to avoid "invalid data" errors
     curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | \
     sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
     
@@ -31,7 +27,6 @@ else
     sudo apt-mark hold kubelet kubeadm kubectl
 fi
 
-# 2. CHECK IF WORKER HAS ALREADY JOINED
 if [ -f /etc/kubernetes/kubelet.conf ]; then
     echo "Worker is already part of a cluster. Skipping join prep..."
 else
